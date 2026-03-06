f81f45fed0c038da7061cebf53ba90af17a4a399
[Alex S] <astanomi@uwaterloo.ca>
2026-03-06 17:32:37 -0500

    photo pins

--- mementoAndroid/app/src/main/java/com/example/mementoandroid/MapActivity.kt
+++ mementoAndroid/app/src/main/java/com/example/mementoandroid/MapActivity.kt
@@ -1,7 +1,6 @@
 package com.example.mementoandroid
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.BitmapShader
 import android.graphics.Canvas
